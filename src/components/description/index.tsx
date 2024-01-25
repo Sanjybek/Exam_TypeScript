b2ea518d33e4d0fdd25a7f31b954e7022f28acd3
Sanjybek <sanjybek003@gmail.com>
2024-01-25 17:11:28 +0600

    description scss

--- src/components/description/index.tsx
+++ src/components/description/index.tsx
@@ -14,14 +14,15 @@ const Description: React.FC<DescDataType> = ({product}) => {
     return (
         <section>
             <div className={s.container} >
-                <div  className={s.info} >
-                    <ul className={s.ul}>  
-                        <h1 className={s.info__title} >{product.title}</h1>
-                        <li className={s.info__card} key={product.id}>
-                            <div className={s.info__description}> {!product.image ? <p></p> : <img className={s.info__picture} src={product.image} alt="" />}Описание {product.description}</div>
-                        </li>   
-                    </ul>
-                </div>
+                <ul className={s.ul}>  
+                    <h1 className={s.info__title} >{product.title}</h1>
+                    <li className={s.info__card} key={product.id}>
+                        <img className={s.info__picture} src={product.image} alt="" />
+                        <p className={s.info__description}> 
+                            <p> Описание </p>
+                        {product.description}</p>
+                    </li>   
+                </ul>
             </div>
         </section>
     );
